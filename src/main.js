288ef47b8cc269bb2ac805ed29dbe9fe21daf825
路飞 <lobve-lf@outlook.com>
2019-08-09 21:34:15 +0800

    '初步配置完成'

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入并配置vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 适配rem
+import 'amfe-flexible'
+
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
